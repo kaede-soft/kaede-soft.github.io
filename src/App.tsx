cb826fbed33e4e4ffb7beeca5111a58144c74a6a
Neil Donnelly <neil@Kaede.local>
2024-01-02 19:58:27 +0000

    updated to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App: React.FC<{}> = ({}) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          yar!
         </p>
         <a
           className="App-link"
